84282b1e2b8227b4ad1e485ddf68036b890bc0a1
EC2 Default User <ec2-user@ip-172-31-41-248.ap-northeast-1.compute.internal>
2019-06-12 02:05:51 +0000

    Finish static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "hello, world! 06/11"
+    render html: "hello, world! 06/12"
   end
 end
